3169aa7024fa763480857f2eb657f0bc91a486de
Brian Dennis <bmd+github@crossjam.net>
2022-02-13 21:14:18 -0500

    Enforce flake8 and pylint conformance

--- 01_hello/hello.py
+++ 01_hello/hello.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python3
+"""
+Author: Brian M. Dennis <brian.dennis@alum.mit.edu>
+Purpose: Say hello
+"""
 
 import click
 
 CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 
+
 @click.command(context_settings=CONTEXT_SETTINGS)
 @click.option('--name', '-n', default="World")
-def main(name):
+def main(name="World"):
     """Say Hello"""
     print(f'Hello, {name}!')
+
 
 if __name__ == '__main__':
     main()
