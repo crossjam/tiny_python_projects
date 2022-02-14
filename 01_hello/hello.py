a57f320b7ef8240dea8559faf847d17fddd21793
Brian Dennis <bmd+github@crossjam.net>
2022-02-13 19:45:45 -0500

    Initial commit of complete solution to task 01.

--- 01_hello/hello.py
+++ 01_hello/hello.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python3
-def main():
-    print('Hello, World!')
+
+import click
+
+CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
+
+@click.command(context_settings=CONTEXT_SETTINGS)
+@click.option('--name', '-n', default="World")
+def main(name):
+    """Say Hello"""
+    print(f'Hello, {name}!')
 
 if __name__ == '__main__':
     main()
